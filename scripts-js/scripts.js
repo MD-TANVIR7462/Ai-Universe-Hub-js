3e483621d21a58816a5b5e5fd9917040073f199c
unknown <mdtanvir7462@gmail.com>
2023-03-02 23:07:18 +0600

    cards-added-by-daynamicaly

--- scripts-js/scripts.js
+++ scripts-js/scripts.js
@@ -1,4 +1,42 @@
 const loadData = () => {
-   fetch.apply()
+   let url = [`https://openapi.programming-hero.com/api/ai/tools`]
+   fetch(url)
+      .then(res => res.json())
+      .then(data => callData(data.data.tools))
 
 }
+function callData(datas) {
+
+   let mainContainer = document.getElementById('main-container')
+   for (const data of datas) {
+      console.log(data)
+      const divs = document.createElement('div')
+      divs.classList.add('col')
+      divs.innerHTML = `
+
+   
+      <div class="card h-100">
+      <p class="rounded-5 p-3">
+      <img src="${data.image}" class="card-img-top" alt="...">
+</p>
+    
+      <div class="card-body">
+        <h5 class="card-title">Features</h5>
+        <p class="text-body fs-6 fw-lighter  fw-light">
+   <span >1.<span >${data.features[0]}</span> </span> <br>
+   <span >2.<span>${data.features[1]}</span> </span> <br>
+   <span >3.<span>${data.features[2]}</span></span> <br>
+   </p>
+      </div>
+      <div class="card-footer">
+        <small class="">
+        <h5 class="card-title">Features</h5>
+        </small>
+      </div>
+
+`
+      mainContainer.appendChild(divs)
+
+   };
+}
+loadData()
